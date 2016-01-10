3c55ab45edabb9400c68e54d4b92843565960384
evenwerk <evenwerk@gmail.com>
2016-01-10 18:53:09 +0100

    Initial Commit

---
+++ tableViewTests/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tableViewTests
+//
+//  Created by Tim Even on 10-01-16.
+//  Copyright © 2016 evenwerk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
