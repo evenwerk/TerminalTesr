3f7fb3a92054cedd31b81f6ef1b92421980a3372
evenwerk <evenwerk@gmail.com>
2016-01-30 20:03:30 +0100

    first commit

---
+++ tableViewTests/IndexTitles.swift
@@ -0,0 +1,21 @@
+//
+//  IndexTitles.swift
+//  tableViewTests
+//
+//  Created by Tim Even on 22-01-16.
+//  Copyright © 2016 evenwerk. All rights reserved.
+//
+
+import UIKit
+
+class IndexTitles: UIControl {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ tableViewTests/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  tableViewTests
+//
+//  Created by Tim Even on 10-01-16.
+//  Copyright © 2016 evenwerk. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
